7c5a042edc2cd80bc19f7bdf09e55435f0cd72a0
joelclementes <joelclementes.dev@gmail.com>
2022-03-09 00:10:38 -0600

    intento con servidor remoto

--- clickseguros.sql
+++ clickseguros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-03-2022 a las 05:07:40
+-- Tiempo de generación: 09-03-2022 a las 06:17:32
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
--- js/App.js
+++ js/App.js
@@ -2,7 +2,9 @@ import {GuardaSolicitudVehiculo} from "../modulos/GuardaSolicitudVehiculo.js"
 class App {
     
     constructor(reset = false) {
-        this.url = "http://127.0.0.1:80/clicksegurosbackend/proceso.php/"
+        this.url = "http://localhost/clicksegurosbackend/proceso.php/";
+        // this.url = "http://a2plcpnl0158:2083/clicksegurosbackend/proceso.php/";
+        
 
         alertify.defaults.transition = "zoom";
         alertify.defaults.theme.ok = "btn btn-primary";
@@ -65,71 +67,12 @@ class App {
             success: function (datos) {
                 let strOpciones = ``;
                 strOpciones = `<option value="0" selected>Seleccione</option>`;
-                // const datos = response.data;
                 for (let d of datos) {
                     strOpciones += `<option value="${d.idSection}">${d.nombreseguro}</option>`;
                 }
                 document.querySelector("#cboTipoDeSeguro").innerHTML = strOpciones;
             }
         })
-    }
-
-    fnConstruyeFrm1(datos){
-        let strOpciones = ``;
-        let datosGenerales = ``;
-        strOpciones = `<option value="0" selected>Seleccione</option>`;
-        // const datos = response.data;
-        for (let d of datos) {
-            strOpciones += `<option value="${d.id}">${d.nombreseguro}</option>`;
-        }
-        datosGenerales = `
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <div class="mb-3">
-                    <label for="txtFecha" class="form-label">Fecha</label>
-                    <input type="date" class="form-control" name="txtFecha" id="txtFecha" placeholder="" value="${this.fechaDeHoy()}">
-                </div>
-                <div class="mb-3">
-                    <label for="txtNombre" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="">
-                </div>
-                <div class="mb-3">
-                    <label for="txtApellidos" class="form-label">Apellidos</label>
-                    <input type="text" class="form-control" name="txtApellidos" id="txtApellidos" placeholder="">
-                </div>
-                <div class="mb-3">
-                    <label for="txtPais" class="form-label">País de residencia</label>
-                    <input type="text" class="form-control" name="txtPais" id="txtPais" placeholder="">
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="mb-3">
-                    <label for="txtCodigoPostal" class="form-label">Código postal</label>
-                    <input type="text" class="form-control" name="txtCodigoPostal" id="txtCodigoPostal" placeholder="">
-                </div>
-                <div class="mb-3">
-                    <label for="txtCelular" class="form-label">Número celular</label>
-                    <input type="text" class="form-control" name="txtCelular" id="txtCelular" placeholder="">
-                </div>
-                <div class="mb-3">
-                    <label for="txtCorreo" class="form-label">Correo</label>
-                    <input type="email" class="form-control" name="txtCorreo" id="txtCorreo" placeholder="correo@ejemplo.com">
-                </div>
-                <div class="mb-3">
-                    <label for="txtCodigoDeEpisodio" class="form-label">Código de episodio</label>
-                    <input type="text" class="form-control" name="txtCodigoDeEpisodio" id="txtCodigoDeEpisodio" placeholder="">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12 col-md-6 mb-3">
-                <label for="cboTipoDeSeguro" class="form-label">Tipo de seguro</label>
-                <select class="form-select" name="cboTipoDeSeguro" id="cboTipoDeSeguro" placeholder="">${strOpciones}</select>
-            </div>
-        </div>`;
-            document.getElementById("datosgenerales").innerHTML = datosGenerales;
-        
-
     }
 
 }
